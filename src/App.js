fefac860c3929be02e5fa16e29f7b147422a8f1e
Obodobright <obodobright0@gmail.com>
2022-05-17 11:24:29 +0100

    Code base Refactoring - Refactored entire codebase by restructuring the folder strutures

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import './App.css'
-import Navbar from './component/Navbar'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages'
-import Products from './pages/Products'
-import Business from './pages/Business'
-import About from './pages/About'
-import Blogs from './pages/blogs'
-import Contact from './pages/contact'
-import ConsentPage from './pages/ConsentPage'
-import 'antd/dist/antd.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./component/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import Products from "./pages/Product";
+import Business from "./pages/Business";
+import About from "./pages/About";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import ConsentPage from "./pages/Consent";
+import "antd/dist/antd.css";
 
 function App() {
   return (
-     <Router>
-        <Navbar />
-        <Routes>
-           <Route exact path='/' element={<Home />} />
-           <Route path='/products' element={<Products />} />
-           <Route path='/business' element={<Business />} />
-           <Route path='/about' element={<About />} />
-           <Route path='/blogs' element={<Blogs />} />
-           <Route path='/contact' element={<Contact />} />
-           <Route path='/ConsentPage' element={<ConsentPage />} />
-        </Routes>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/business" element={<Business />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/ConsentPage" element={<ConsentPage />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
